f6b17012adb566246fd415a0eedb23b9ceffc371
androidev <alvaro.andrade.a@mail.pucv.cl>
2020-06-04 14:13:11 -0400

    test-side-bar

--- packages/panel/src/App.vue
+++ packages/panel/src/App.vue
@@ -1,10 +1,20 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <div class="sideBar">
+        <ul>
+          <li>
+            <router-link to="/">Home</router-link>
+          </li>
+          <li>
+          <router-link to="/about">About</router-link>
+          </li>
+        </ul>
+      </div>
+      <div class="content">
+        <router-view />
+      </div>
     </div>
-    <router-view />
   </div>
 </template>
 
@@ -27,6 +37,21 @@
     &.router-link-exact-active {
       color: #42b983;
     }
+  }
+  .sideBar {
+    position: fixed;
+    height: 100%;
+    background: red;
+    width: 25%;
+    top: 0;
+    left: 0;
+    overflow-x: hidden;/* Disable horizontal scroll */
+    z-index: 2;
+    padding-top: 60px;
+    transition: 0.5s;
+  }
+  .content{
+    margin-left: 25%;
   }
 }
 </style>
