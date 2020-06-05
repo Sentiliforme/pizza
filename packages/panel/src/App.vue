149bcf1d4c470c8fa2ecace10bd1122edf4fb003
androidev <alvaro.andrade.a@mail.pucv.cl>
2020-06-04 22:34:44 -0400

    side-bar

--- packages/panel/src/App.vue
+++ packages/panel/src/App.vue
@@ -1,34 +1,25 @@
 <template>
   <v-app>
-    <v-card >
-      <v-navigation-drawer v-model="drawer" :mini-variant.sync="mini" permanent>
+    <v-sheet height="100%">
+      <v-container>
+        <H1>HOLA</H1>
+        <router-view />
+      </v-container>
+
+      <v-navigation-drawer v-model="drawer" absolute>
         <v-list-item class="px-2">
-          <v-list-item-avatar>
-            <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
-          </v-list-item-avatar>
-
-          <v-list-item-title>John Leider</v-list-item-title>
-
+          <v-list-item-title>Secciones</v-list-item-title>
           <v-btn icon @click.stop="mini = !mini">
             <v-icon>mdi-chevron-left</v-icon>
           </v-btn>
         </v-list-item>
-
-        <v-divider></v-divider>
-
-        <v-list dense>
-          <v-list-item v-for="item in items" :key="item.title" link>
-            <v-list-item-icon>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-icon>
-
-            <v-list-item-content>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-content>
+            <v-list-item-title>{{item.title}}</v-list-item-title>
+          </v-list-content>
+        </v-list-item>
       </v-navigation-drawer>
-    </v-card>
+    </v-sheet>
   </v-app>
 </template>
 <script>
@@ -38,11 +29,11 @@ export default {
     return {
       drawer: true,
       items: [
-        { title: 'Home', icon: 'mdi-home-city' },
-        { title: 'My Account', icon: 'mdi-account' },
-        { title: 'Users', icon: 'mdi-account-group-outline' }
+        { title: 'Categoria', icon: 'mdi-home-city' },
+        { title: 'Productos', icon: 'mdi-account' },
+        { title: 'Ingredientes', icon: 'mdi-account-group-outline' }
       ],
-      mini: true
+      mini: true,
     }
   },
   name: 'App',
@@ -61,6 +52,5 @@ export default {
   display: grid;
   height: 100%;
   width: 100%;
-
 }
 </style>
--- packages/panel/src/components/SideBar.vue
+++ packages/panel/src/components/SideBar.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card>
-    <v-navigation-drawer v-model="drawer" :mini-variant.sync="mini" permanent >
+    <v-navigation-drawer v-model="drawer" :mini-variant.sync="mini" permanent>
       <v-list-item class="px-2">
         <v-list-item-avatar>
           <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
@@ -9,7 +9,7 @@
         <v-list-item-title>John Leider</v-list-item-title>
 
         <v-btn icon @click.stop="mini = !mini">
-          <v-icon>mdi-chevron-left</v-icon>
+          <v-icon>arrow_downward</v-icon>
         </v-btn>
       </v-list-item>
 
@@ -40,9 +40,9 @@ export default {
         { title: 'My Account', icon: 'mdi-account' },
         { title: 'Users', icon: 'mdi-account-group-outline' }
       ],
-      mini: true,
+      mini: true
     }
-  },
+  }
 }
 </script>
 
