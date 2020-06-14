7f6a139473d7b59a75ec8e1fcd7bb958c1874a46
Martin Santibanez <martin.santibanez.a@gmail.com>
2020-06-14 12:18:56 +0200

    cart summary design

--- packages/front/src/components/general/CartSummary.tsx
+++ packages/front/src/components/general/CartSummary.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './CartSummary.scss'
+import { ReactComponent as ShoppingIcon } from '../../assets/svg/shopping-cart-solid.svg'
 
 function CartSummary() {
   return (
     <div className="cart-summary">
-      <div className="total-price">$9.990</div>
-      <div className="continue-button">CONTINUAR</div>
+      <div className="left">
+        <ShoppingIcon />
+        <div className="total-price">$9.990</div>
+      </div>
+      <div className="right">
+        <button className="continue-button">CONTINUAR ></button>
+      </div>
     </div>
   )
 }
